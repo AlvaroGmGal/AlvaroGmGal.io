aa5a0682f2dc2c91ed606635eb95c7442e14b270
Alvaro <alvarogomezgalindo_8192@escuelaces.net>
2022-11-02 14:19:16 +0100

    nuevos apuntes

---
+++ Hola.java
@@ -0,0 +1,11 @@
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        int i = 3;
+        if (i < 5) {
+            System.out.println("Es menor");
+        } else {
+            System.out.println("Es igual o mayor");
+        }
+    }
+}
